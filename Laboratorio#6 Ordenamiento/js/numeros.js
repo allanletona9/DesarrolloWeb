d8f558595af71354047903f14ad9192198aad406
allanletona9 <39815030+allanletona9@users.noreply.github.com>
2019-09-12 19:49:57 -0600

    Add files via upload

--- Laboratorio#6 Ordenamiento/js/numeros.js
+++ Laboratorio#6 Ordenamiento/js/numeros.js
@@ -65,18 +65,11 @@ document.getElementById('resultado').innerHTML = JSON.stringify(randomArr, 0, 4)
     }
 
     function burbuja(arreglo){
-    //recorreremos todos los elementos hasta n-1
     for(i=0;i<(arreglo.length-1);i++)
-    //recorreremos todos los elementos hasta n-i, tomar en cuenta los ultimos no tiene caso ya que ya estan acomodados.
     for(j=0;j<(arreglo.length-i);j++){
- 
-        //comparamos
         if(arreglo[j]>arreglo[j+1]){
-             //guardamos el numero mayor en el auxiliar
              aux=arreglo[j];
-             //guardamos el numero menor en el lugar correspondiente
              arreglo[j]=arreglo[j+1];
-             //asignamos el auxiliar en el lugar correspondiente
              arreglo[j+1]=aux;
         }
     }

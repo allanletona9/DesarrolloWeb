18af1061e388958db6531add53a9de69916ee930
allanletona9 <39815030+allanletona9@users.noreply.github.com>
2019-09-19 19:44:47 -0600

    Ultima versión Calculadora

--- Laboratorio#7/js/script.js
+++ Laboratorio#7/js/script.js
@@ -1,9 +1,10 @@
+/*Imprime el numero dependiendo del parametro que esta recibiendo del html*/
 function imprimirValores(numero)
 {
-    document.calculator.ans.value+=numero;
+    document.calculator.ans.value+=numero; /*Funcion predeterminada de js para poder imprimir valores*/
 }
 
 function resultado()
 {
-    document.calculator.ans.value=eval(document.calculator.ans.value);
+    document.calculator.ans.value=eval(document.calculator.ans.value); /*Funcion predeterminada para realizar el calculo de la operacion*/
 }
